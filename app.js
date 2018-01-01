48d39802c6b71c540cb4258c2640b47b553b1446
Jonas A <jonas.ahrens@gmail.com>
2018-01-01 02:06:06 +0100

    Clean

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 // Require
-const express = require('express');
-const flexjson = require('jsonflex')();
-const compression = require('compression');
+const express = require('express')
+const flexjson = require('jsonflex')()
+const compression = require('compression')
 
-const app = express();
+const app = express()
 
-app.use(compression());
-app.use(flexjson);
-app.use(express.static('www'));
+app.use(compression())
+app.use(flexjson)
+app.use(express.static('public'))
 
+/* eslint-disable */
 app.get(/^[^\.]*$/, (req, res) => {
-  res.sendFile(__dirname + '/www/index.html');
-});
+  res.sendFile(__dirname + '/public/index.html')
+})
+/* eslint-enable */
 
 // Start server
 app.listen(3000, () =>
   console.log('Webserver listening on port 3000')
-);
+)
