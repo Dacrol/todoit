e0b72139762bb1f6cd7b7c1ae7df215c7a757115
Jonas A <jonas.ahrens@gmail.com>
2018-01-02 23:12:16 +0100

    Add item selection and editing functionality

--- public/js/main.js
+++ public/js/main.js
@@ -17,3 +17,43 @@ const itemGrids = [];
 [].push.apply(itemGrids, $('.list-column-content').get().map((container) => {
   return new List(container, itemGrids, listsGrid)
 }))
+
+var editing = false
+
+$('.list-item-content').each(function () { this.addEventListener('input', () => { itemGrids.forEach((grid) => { grid.refreshItems().layout() }) }) })
+
+$('.list-item').click(function () {
+  $(this).addClass('frozen')
+  $(this).children().first().focus()
+  editing = true
+})
+
+$('.list-item').mousedown(function (event) {
+  if (!$(this).hasClass('frozen')) {
+    event.preventDefault()
+    if (editing) {
+      $('.frozen').each(function () { $(this).children().first().blur() })
+      editing = false
+    }
+  }
+})
+
+$('.list-item-content').focusout(function () {
+  if (editing) {
+    console.log($(this))
+    $(this).parent().removeClass('frozen')
+    clearSelect()
+    editing = false
+  }
+})
+
+/**
+ * Clears any selections
+ */
+function clearSelect () {
+  if (window.getSelection) {
+    window.getSelection().removeAllRanges()
+  } else if (document.selection) {
+    document.selection.empty()
+  }
+}
